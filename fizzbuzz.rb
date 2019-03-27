8f897ef2a99ec67c199b088a02014c69ea223b5f
voluble-bug-0219 <montensia1@outlook.com>
2019-03-27 02:43:42 +0000

    Automatically backed up by Learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -2,12 +2,12 @@
 # See README.md for instructions on how to do this
 require_relative './spec/fizzbuzz_spec.rb'
 def fizzbuzz(num)
-  if(num % 3 == 0)
-    return "Fizz"
+  if(num % 3 == 0 && num % 5 == 0)
+    return "FizzBuzz"
   elsif(num % 5 == 0)
     return "Buzz"
-  elsif(num % 3 == 0 && num % 5 == 0)
-    return "FizzBuzz"
+  elsif(num % 3 == 0)
+    return "Fizz"
   else
     return nil;
   end
