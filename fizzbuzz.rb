1c61fa0a432c269d95c43797a230658c410e8db3
voluble-bug-0219 <montensia1@outlook.com>
2019-03-27 02:36:07 +0000

    Automatically backed up by Learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,6 +1,6 @@
 # Don't forget! This file needs to be 'required' in its spec file
 # See README.md for instructions on how to do this
-require 'spec/fizzbuzz_spec.rb'
+require_relative './spec/fizzbuzz_spec.rb'
 def fizzbuzz(num)
   if(num % 3 == 0)
     return "Fizz"
